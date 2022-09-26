e32c48d827933591c882dd2372e065e2ad720a26
quanscheng <quanscheng@163.com>
2022-09-26 18:00:25 +0800

    feat: 新增reportWebVitals

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+reportWebVitals()
